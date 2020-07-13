9373c66656b797203f9aae3b3f3679a65baed37b
bernard.spiegl <@bernard.spiegl>
2020-07-13 15:53:47 +0200

    gui update added enabling disabling based on connection status

--- app.py
+++ app.py
@@ -12,16 +12,50 @@ from enum import Enum
 from concurrent.futures import ThreadPoolExecutor
 
 
+class Worker(QtCore.QRunnable):
+
+    def __init__(self, fn, *args, **kwargs):
+        super(Worker, self).__init__()
+        self.fn = fn
+        self.args = args
+        self.kwargs = kwargs
+        self.signals = WorkerSignals()
+        self.kwargs['progress_callback'] = self.signals.progress
+
+    def run(self):
+        try:
+            result = self.fn()
+        except:
+            traceback.print_exc()
+            exctype, value = sys.exc_info()[:2]
+            self.signals.error.emit((exctype, value, traceback.format_exc()))
+        else:
+            self.signals.result.emit(result)
+        finally:
+            self.signals.finished.emit()
+
+
+class WorkerSignals(QtCore.QObject):
+    finished = Signal()
+    error = Signal(tuple)
+    result = Signal(object)
+    progress = Signal(int)
+
+
 class Application(QMainWindow):
+    threadpool = QThreadPool()
     executor = ThreadPoolExecutor(max_workers=1)
+    connected = False
 
     def __init__(self, parent=None):
         QMainWindow.__init__(self, parent)
 
-        self.mainWidget = QStackedWidget()
+        self.stackedMainWidget = QStackedWidget()
+
+        self.mainWidget = QWidget()
 
         self.ConnectWidget = ConnectWidget()
-        self.ConnectWidget.button.clicked.connect(self._connect)
+        self.ConnectWidget.button.clicked.connect(self._connect_disconnect)
 
         self.ReadCoilsWidget = ReadCoilsWidget()
         self.ReadCoilsWidget.dropdown.view().pressed.connect(self.change_widget)
@@ -30,58 +64,58 @@ class Application(QMainWindow):
         self.ReadDiscreteInputsWidget.dropdown.view().pressed.connect(self.change_widget)
 
         self.DefaultWidget = DefaultWidget()
+
         self.DefaultWidget.dropdown.view().pressed.connect(self.change_widget)
 
-        self.mainWidget.addWidget(self.ConnectWidget)
-        self.mainWidget.addWidget(self.DefaultWidget)
-        self.mainWidget.addWidget(self.ReadCoilsWidget)
-        self.mainWidget.addWidget(self.ReadDiscreteInputsWidget)
-        self.connect_buttons()
+        self.stackedMainWidget.addWidget(self.DefaultWidget)
+        self.stackedMainWidget.addWidget(self.ReadCoilsWidget)
+        self.stackedMainWidget.addWidget(self.ReadDiscreteInputsWidget)
+        self.stackedMainWidget.setEnabled(self.connected)
 
+        layout = QVBoxLayout()
+        layout.addWidget(self.ConnectWidget)
+        layout.addWidget(self.stackedMainWidget)
+
+        self.mainWidget.setLayout(layout)
         self.setCentralWidget(self.mainWidget)
 
-    def _connect(self):
-        serialized_thread = Thread(target=serializer.start)
-        check_connection_thread = Thread(target=asyncio.new_event_loop().run_until_complete(self.check_connection()))
-        serialized_thread.start()
-        check_connection_thread.start()
-
-    def _dc(self):
-        serializer.req_queue.put("DC")
-        self.mainWidget.setCurrentWidget(self.ConnectWidget)
-
-    def connect_buttons(self):
-        i = 0
-        while w := self.mainWidget.widget(i):
-            try:
-                w.DCButton.clicked.connect(self._dc)
-            except AttributeError as e:
-                print(e)
-            finally:
-                i += 1
+    def _connect_disconnect(self):
+        if not self.connected:
+            serializer_thread = Thread(target=serializer.start)
+            check_connection_thread = Thread(
+                target=lambda: asyncio.new_event_loop().run_until_complete(self.check_connection()))
+            serializer_thread.start()
+            check_connection_thread.start()
+        else:
+            serializer.req_queue.put("DC")
+            self.connected = False
+            self.stackedMainWidget.setEnabled(self.connected)
+            self.ConnectWidget.button.setText("Connect")
 
     def change_widget(self):
-        current = str(self.mainWidget.currentWidget().dropdown.currentText())
+        current = str(self.stackedMainWidget.currentWidget().dropdown.currentText())
         if current == "READ COILS":
             print("read coils")
-            self.mainWidget.setCurrentWidget(self.ReadCoilsWidget)
-            self.mainWidget.currentWidget().dropdown.setCurrentText("READ COILS")
+            self.stackedMainWidget.setCurrentWidget(self.ReadCoilsWidget)
+            self.stackedMainWidget.currentWidget().dropdown.setCurrentText("READ COILS")
         elif current == "READ DISCRETE INPUTS":
             print("read discrete")
-            self.mainWidget.setCurrentWidget(self.ReadDiscreteInputsWidget)
-            self.mainWidget.currentWidget().dropdown.setCurrentText("READ DISCRETE INPUTS")
+            self.stackedMainWidget.setCurrentWidget(self.ReadDiscreteInputsWidget)
+            self.stackedMainWidget.currentWidget().dropdown.setCurrentText("READ DISCRETE INPUTS")
         elif current == "READ HOLDING REGISTERS":
             print("read holding")
         else:
             print("else")
-            self.mainWidget.setCurrentWidget(self.DefaultWidget)
+            self.stackedMainWidget.setCurrentWidget(self.DefaultWidget)
 
     async def check_connection(self):
-        ack = await asyncio.get_event_loop().run_in_executor(self.executor, self.wait_for_ack)
+        ack = await asyncio.get_event_loop().run_in_executor(self.executor, self._wait_for_ack)
         if ack == "ACK":
-            self.mainWidget.setCurrentWidget(self.DefaultWidget)
+            self.connected = True
+            self.stackedMainWidget.setEnabled(self.connected)
+            self.ConnectWidget.button.setText("Disconnect")
 
-    def wait_for_ack(self):
+    def _wait_for_ack(self):
         try:
             message = serializer.res_queue.get()
             return message
@@ -90,7 +124,7 @@ class Application(QMainWindow):
 
 
 def run_gui():
-    app = QApplication(sys.argv)
+    app = QApplication()
     app.setStyle('Fusion')
     mainWindow = Application()
     mainWindow.show()
--- style/widgets.py
+++ style/widgets.py
@@ -9,6 +9,9 @@ from enum import Enum
 from style.custom_elements import *
 from concurrent.futures import ThreadPoolExecutor
 
+protocol_code = '0000'
+unit_address = '01'
+
 
 class Codes(Enum):
     READ_COILS = '01'
@@ -24,7 +27,6 @@ class Codes(Enum):
 
 
 def clear_line(lineedit):
-
     if lineedit.text() == lineedit.default_value:
         lineedit.clear()
 
@@ -34,36 +36,52 @@ class ConnectWidget(QWidget):
     def __init__(self, parent=None):
         super(ConnectWidget, self).__init__(parent, QtCore.Qt.Window)
         self.button = QPushButton("Connect")
-        self.dropdown = QComboBox(self)
-        self.dropdown.setEnabled(False)
-        self.dropdown.addItems([x.name.replace('_', ' ') for x in Codes])
-        self.layout = QFormLayout()
-        self.layout.addWidget(self.button)
-        self.layout.addRow("Function: ", self.dropdown)
-        self.setLayout(self.layout)
+        layout = QVBoxLayout()
+        layout.addWidget(self.button)
+        self.setLayout(layout)
 
 
 class DefaultWidget(QWidget):
+    executor = ThreadPoolExecutor()
 
     def __init__(self, parent=None):
         super(DefaultWidget, self).__init__(parent, QtCore.Qt.Window)
-        self.DCButton = QPushButton("Disconnect")
         self.sendButton = QPushButton("SEND")
-        self.sendButton.clicked.connect(self.validate_and_send)
         self.dropdown = QComboBox(self)
         self.dropdown.addItems([x.name.replace('_', ' ') for x in Codes])
         self.layout = QFormLayout()
-        self.layout.addWidget(self.DCButton)
         self.layout.addRow("Function: ", self.dropdown)
         self.setLayout(self.layout)
 
-    def validate_and_send(self):
-        serializer.req_queue.put(self.edit.text())
-        asyncio.get_event_loop().run_until_complete(self.put_message())
 
-    async def put_message(self):
+class DefaultRWWidget(DefaultWidget):
+    last_id = 0
+
+    def __init__(self):
+        super(DefaultRWWidget, self).__init__()
+        self.firstAddress = ClickableLineEdit("0")
+        self.count = ClickableLineEdit("1")
+        self.sendButton.clicked.connect(self.validate_and_send)
+        self.count.clicked.connect(lambda: clear_line(self.count))
+        self.firstAddress.clicked.connect(lambda: clear_line(self.firstAddress))
+
+    def validate_and_send(self):
+        hex_id_stripped = str(hex(self.last_id))[2:]
+        first_address_stripped = str(hex(int(self.firstAddress.text())))[2:]
+        count_stripped = str(hex(int(self.count.text())))[2:]
+        message = '0' * (4 - len(hex_id_stripped)) + hex_id_stripped + protocol_code + '0006' + unit_address + getattr(
+            Codes, self.dropdown.currentText().replace(' ', '_')).value + '0' * (
+                          4 - len(first_address_stripped)) + first_address_stripped + '0' * (
+                              4 - len(count_stripped)) + count_stripped
+        print(message)
+        serializer.req_queue.put(message)
+        asyncio.get_event_loop().run_until_complete(self.show_response())
+
+    async def show_response(self):
         message = await asyncio.get_event_loop().run_in_executor(self.executor, self.get_message)
-        self.messageLabel.setText(str(message))
+        self.messageLabel = QLabel(str(message))
+        self.layout.addWidget(self.messageLabel)
+        self.setLayout(self.layout)
 
     def get_message(self):
         try:
@@ -71,16 +89,6 @@ class DefaultWidget(QWidget):
             return message
         except queue.Empty:
             return
-
-
-class DefaultRWWidget(DefaultWidget):
-
-    def __init__(self):
-        super(DefaultRWWidget, self).__init__()
-        self.firstAddress = ClickableLineEdit("0")
-        self.count = ClickableLineEdit("1")
-        self.count.clicked.connect(lambda: clear_line(self.count))
-        self.firstAddress.clicked.connect(lambda: clear_line(self.firstAddress))
 
 
 class ReadCoilsWidget(DefaultRWWidget):
@@ -110,72 +118,3 @@ class ReadHoldingRegistersWidget(DefaultRWWidget):
         self.layout.addWidget(self.sendButton)
         self.setLayout(self.layout)
 
-
-class CentralWidget(QWidget):
-
-    def __init__(self, parent=None):
-        super(CentralWidget, self).__init__(parent, QtCore.Qt.Window)
-
-        self.requestLabel = QLabel("REQUEST")
-        self.requestLabel.setAlignment(QtCore.Qt.AlignCenter)
-
-        self.edit = ClickableLineEdit("abcd")
-        self.edit.clicked.connect(self.clear_line)
-
-        self.styleComboBox = QComboBox()
-
-        self.preview = QLabel("PREVIEW")
-        self.preview.setAlignment(QtCore.Qt.AlignCenter)
-
-        self.button = QPushButton("Send Data")
-        self.button.clicked.connect(self.change_layout)
-
-        self.dropdown = QtWidgets.QComboBox(self)
-        self.dropdown.addItems([x.name.replace('_', ' ') for x in Codes])
-
-        self.separator = QFrame()
-        self.separator.setGeometry(QRect(320, 150, 118, 3))
-        self.separator.setFrameShape(QFrame.HLine)
-        self.separator.setFrameShadow(QFrame.Sunken)
-
-        self.responseLabel = QLabel("RESPONSE")
-        self.responseLabel.setAlignment(QtCore.Qt.AlignCenter)
-
-        self.messageLabel = QLabel()
-        self.messageLabel.setAlignment(QtCore.Qt.AlignCenter)
-
-        layout2 = QFormLayout()
-
-        layout2.addRow("Function: ", self.dropdown)
-        layout2.addWidget(self.button)
-
-        self.layout = QFormLayout()
-
-        self.layout.addWidget(self.requestLabel)
-        self.layout.addRow("Function: ", self.dropdown)
-        self.layout.addWidget(self.edit)
-        self.layout.addWidget(self.button)
-        self.layout.addWidget(self.preview)
-        self.layout.addWidget(self.separator)
-        self.layout.addWidget(self.responseLabel)
-        self.layout.addWidget(self.messageLabel)
-
-        self.setLayout(self.layout)
-
-    def validate_and_send(self):
-        serializer.req_queue.put(self.edit.text())
-        asyncio.get_event_loop().run_until_complete(self.put_message())
-
-    async def put_message(self):
-        message = await asyncio.get_event_loop().run_in_executor(self.executor, self.get_message)
-        self.messageLabel.setText(str(message))
-
-    def get_message(self):
-        try:
-            message = serializer.res_queue.get()
-            return message
-        except queue.Empty:
-            return
-
-    def change_layout(self):
-        self.setLayout(self.layout)
